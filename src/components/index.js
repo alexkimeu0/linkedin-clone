da4701aea81f5753c23e5c3ad08d4c0423d2eb10
alexkimeu0 <kim3ual3x@gmail.com>
2020-12-31 17:12:20 +0300

    Created the Header component

---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Header } from './Header/Header'
+export { default as HeaderOption } from './Header/HeaderOption/HeaderOption'
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD6fkQiMxvt1emwwMkltaQ2eNVJn6xnNuQ",
+    authDomain: "linkedin-99453.firebaseapp.com",
+    projectId: "linkedin-99453",
+    storageBucket: "linkedin-99453.appspot.com",
+    messagingSenderId: "775669008914",
+    appId: "1:775669008914:web:008a041940d1325ac566cb"
+  };
